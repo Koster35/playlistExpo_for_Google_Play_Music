b870c59fa790bc3d93acdad1c4fdcb7736044ebf
Maxwell Koster <Koster.35@osu.edu>
2018-08-01 22:54:51 -0400

    Finished primary functionality of content_script.js;
    content_script.js now exports number, title, duration, artist, album,
    and play count to the .csv file.
    
    Does not correctly export rating.

--- content_script.js
+++ content_script.js
@@ -1,18 +1,17 @@
+var ratingMap = {
+    '5' : "Like",
+    '3': "Dislike"
+};
 
-
-
-//var allSongRows = $( "tr.song-row" );
-//$( "div.songlist-container > table.song-table" ).find( allSongRows );
-
-
-console.log("TEST");
-let csvContent = "data:text/csv;charset=utf-8,";
-var allCells = $( "tr.song-row span.column-content" );
-$( "table.song-table tr.song-row" ).each(function( index ) {
-	  let row = $(this).find(allCells).toArray().join(",");
-	  csvContent += row + "\r\n";
+var csvContent = "data:text/csv;charset=utf-8,";
+var allCells = $( "span" ); // Creates collection of all the cells that will written to the .csv
+$( "table.song-table tr.song-row" ).each(function() {
+	$(this).find(allCells).each(function () { 
+		csvContent += $(this).text() + ",";
+		});
+	console.log($("[col='rating']").data("rating"));
+	csvContent += ratingMap[$(this).find("[data-col='rating']").data("data-rating")] + "\r\n";
 	});
-
 var encodedUri = encodeURI(csvContent);
 var link = document.createElement("a");
 link.setAttribute("href", encodedUri);
