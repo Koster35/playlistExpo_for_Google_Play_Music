dae88348efb58dad0b5d2a7b69adc188e67f88e2
Maxwell Koster <Koster.35@osu.edu>
2018-09-27 18:36:05 -0400

    Several bugfixes
    Bug fix: In the output file, ratings appear in the play-count column if no play-count exists.
    Bug fix: Undefined is printed at the end of some lines of the output file.
    New bug: Some non-standard English characters are being printed to the output file incorrectly.

--- content_script.js
+++ content_script.js
@@ -2,24 +2,36 @@
 @title GPM Playlist Exporter
 @author Maxwell L Koster
 */
+
+// Maps extracted rating integers to English strings.
 var ratingMap = {
     '5' : "Like",
-    '3': "Dislike"
+    '1': "Dislike",
+    '0': "",
+    'null': ""
 };
-// Appends file header and column headers to output String
-var csvContent = "data:text/csv;charset=utf-8,Index,Title,Duration,Artist,Album,Play-count,Rating\r\n"; 
+// Appends file header and column headers to output String.
+var csvContent = "data:text/csv;charset=utf-8,Index,Title,Duration,Artist,Album,Play-count,Rating\n"; 
 var playlistTitle = $("div.title-row > h2").text();
-// Creates collection of all the cells that will written to the .csv
-var allCells = $( "span" ); 
-// Extracts data for each song (row) and appends to output String
+// Creates collection of all the span elements that will written to the output file.
+var spanCells = $( "span" );
+// Extracts data for each song (row) and appends to output String.
 $( "table.song-table tr.song-row" ).each(function() {
-	$(this).find(allCells).each(function () { 
-		csvContent += "\"" +$(this).text() + "\",";
-		});
-	console.log($("[data-col='rating']").attr("data-rating"));
-	csvContent += ratingMap[$(this).find("[data-col='rating']").attr("data-rating")] + "\r\n";
+	var colIndex = 0;
+	// Appends span element contents, if contents exist, to the file string.
+	$(this).find(spanCells).each(function () { 
+		csvContent += "\"" + $(this).text() + "\",";
+		colIndex++;
 	});
-// Encodes file String and prompts user to download file
+	// Appends play-count "0", if no play-count span was found, to file string.
+	if (colIndex == 5) {
+		csvContent += "0,";
+	}
+	// Appends rating to file string.
+	csvContent += ratingMap[$(this).find( "[data-col='rating']" ).attr( "data-rating" )];
+	csvContent += "\n";
+});
+// Encodes file String and prompts user to download .csv output file.
 var encodedUri = encodeURI(csvContent);
 var link = document.createElement("a");
 link.setAttribute("href", encodedUri);
